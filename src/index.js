56b7202b10a2860a45de238faa9897a063bc78c5
Ricky_Zeng <zengchiwai08@gmail.com>
2026-01-05 15:03:17 +0800

    Done the home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import "./style.css";
 
-console.log("Hello World!");
+function cleanContent(){
+    const containerDiv = document.querySelector("#content");
+    while(containerDiv.firstChild){
+        containerDiv.removeChild(containerDiv.firstChild);
+    }
+}
+
+function loadHomePage(){
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("home-title");
+
+    titleDiv.textContent = "Lorem ipsum dolor sit amet.";
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("home-image");
+
+    const div = document.createElement("div");
+    div.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum delectus eum ipsum ab quia nulla eafuga quis eos?";
+
+    const containerDiv = document.querySelector("#content");
+    containerDiv.classList.add("home");
+    containerDiv.appendChild(titleDiv);
+    containerDiv.appendChild(imageDiv);
+    containerDiv.appendChild(div);
+}
+
+cleanContent();
+loadHomePage();
+
